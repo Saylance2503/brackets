038e7968108521ca3355e5a83740ee8c83301f87
Saylance2503 <yurik_05@inbox.ru>
2020-10-15 19:02:01 +0400

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [],
+        st = str;
+    
+    bracketsConfig = bracketsConfig.flat().join('');
+
+    for ( let j = 0; j <= bracketsConfig.length; j += 2) {
+        console.log(array.push(bracketsConfig.slice(j, j + 2)));
+    }
+    
+    for (let i = 0; i <= str.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            st = st.replace(array[j], '');
+        }
+    }
+    return (st === '') ? true : false;
 }
